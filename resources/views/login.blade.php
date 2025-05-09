dd84d039d444121e6a50c531c98ff3fb106c737b
Xcasterz <xct.xcaster@gmail.com>
2025-05-09 08:49:53 +0700

    edit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -49,6 +49,11 @@
                 class="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors">
                 เข้าสู่ระบบ
             </button>
+
+            <div class="mt-4 text-center">
+                <a href="{{ route('register.show') }}" class="text-blue-600 hover:text-blue-800 text-sm">ยังไม่มีบัญชี?
+                    สมัครสมาชิกที่นี่</a>
+            </div>
         </form>
     </div>
 
